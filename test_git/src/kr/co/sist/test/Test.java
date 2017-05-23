b06b2f75507eb70a98ec81e733db7eb65e30be09
user <user@user-PC>
2017-05-23 11:49:01 +0900

    branch로 수정

--- test_git/src/kr/co/sist/test/Test.java
+++ test_git/src/kr/co/sist/test/Test.java
@@ -5,6 +5,9 @@ public class Test {
 	public static void main(String[] args) {
 		int i=0;
 		System.out.println("변수 i = "+i);
+		//jang가 수정함.
+		int j=0;
+		System.out.println("j="+j);
 	}//main
 
 }//class
